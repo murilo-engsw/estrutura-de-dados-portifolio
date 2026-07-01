ea5b6974f095d1d6188737c7e71f1d21c289379e
Murilo Dahmer da Silva <murilodahmer@alunos.utfpr.edu.br>
2026-07-01 17:42:28 -0300

    feat: corrigindo aula-06 (1)

--- aula-06/aula-06-vetor.c
+++ aula-06/aula-06-vetor.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
-
-    int numeros[5];
+int main() {
+    int vetor[5];
     int soma = 0;
 
-    printf("Digite 5 numeros inteiros:\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Numero %d: ", i + 1);
-        scanf("%d", &numeros[i]);
-
-        soma += numeros[i];
+    printf("--- Leitura e Processamento de Vetor ---\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Digite o valor para a posicao %d: ", i);
+        scanf("%d", &vetor[i]);
+        soma += vetor[i]; // Utilizando variavel acumuladora
     }
 
-    printf("\nElementos do vetor:\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ", numeros[i]);
+    printf("\nElementos armazenados no vetor: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", vetor[i]);
     }
+    
+    printf("\nSoma total dos elementos: %d\n", soma);
 
-    printf("\n\nSoma dos elementos: %d\n", soma);
+    /* Nota: O laco for vai de 0 a 4. Tentar acessar vetor[5] causaria
+       comportamento indefinido (acesso invalido a memoria). */
 
     return 0;
 }
