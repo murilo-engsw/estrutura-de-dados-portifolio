b7cfd759f5cf86e653d754e6f648a8782c80af1d
Murilo Dahmer da Silva <murilodahmer@alunos.utfpr.edu.br>
2026-07-01 17:47:48 -0300

    feat: corrigindo aula-11

--- aula-11/aula-11.c
+++ aula-11/aula-11.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct No
-{
+typedef struct No {
     int info;
     struct No *ant;
     struct No *prox;
 } No;
 
-No *inserirFinal(No *lista, int valor)
-{
+No* inserirInicio(No *lista, int valor) {
+    No *novo = (No*) malloc(sizeof(No));
+    novo->info = valor;
+    novo->ant = NULL;
+    novo->prox = lista;
 
-    No *novo = (No *)malloc(sizeof(No));
+    if (lista != NULL) {
+        lista->ant = novo;
+    }
 
+    return novo;
+}
+
+No* inserirFinal(No *lista, int valor) {
+    No *novo = (No*) malloc(sizeof(No));
     novo->info = valor;
     novo->ant = NULL;
     novo->prox = NULL;
 
-    if (lista == NULL)
-    {
+    if (lista == NULL) {
         return novo;
     }
 
     No *p = lista;
-
-    while (p->prox != NULL)
-    {
+    while (p->prox != NULL) {
         p = p->prox;
     }
 
@@ -35,45 +41,54 @@ No *inserirFinal(No *lista, int valor)
     return lista;
 }
 
-void imprimir(No *lista)
-{
-
+void imprimirDireta(No *lista) {
     No *p = lista;
-
     printf("Ordem direta: ");
-
-    while (p != NULL)
-    {
+    while (p != NULL) {
         printf("%d ", p->info);
-
-        if (p->prox == NULL)
-            break;
-
         p = p->prox;
     }
-
-    printf("\nOrdem inversa: ");
-
-    while (p != NULL)
-    {
-        printf("%d ", p->info);
-        p = p->ant;
-    }
-
     printf("\n");
 }
 
-int main()
-{
+void imprimirReversa(No *lista) {
+    if (lista == NULL) {
+        return;
+    }
 
+    No *p = lista;
+    while (p->prox != NULL) {
+        p = p->prox;
+    }
+
+    printf("Ordem inversa: ");
+    while (p != NULL) {
+        printf("%d ", p->info);
+        p = p->ant;
+    }
+    printf("\n");
+}
+
+void liberarLista(No *lista) {
+    No *p = lista;
+    while (p != NULL) {
+        No *temp = p->prox;
+        free(p);
+        p = temp;
+    }
+}
+
+int main() {
     No *lista = NULL;
 
-    lista = inserirFinal(lista, 10);
     lista = inserirFinal(lista, 20);
     lista = inserirFinal(lista, 30);
+    lista = inserirInicio(lista, 10);
     lista = inserirFinal(lista, 40);
 
-    imprimir(lista);
+    imprimirDireta(lista);
+    imprimirReversa(lista);
 
+    liberarLista(lista);
     return 0;
 }
