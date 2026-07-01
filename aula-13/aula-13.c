8c95a2441cf4834a566eb9a89544b4ad389aade3
Murilo Dahmer da Silva <murilodahmer@alunos.utfpr.edu.br>
2026-07-01 17:50:33 -0300

    feat: corrigindo aula-13

--- aula-13/aula-13.c
+++ aula-13/aula-13.c
@@ -1,28 +1,63 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int vetor[5];
+    int maior;
+    int somaPositivos = 0;
+    int numTabuada;
+    int numContagem;
 
-    int numeros[5];
+    printf("--- Leitura e Processamento de Vetor ---\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Digite o %d numero: ", i + 1);
+        scanf("%d", &vetor[i]);
+        
+        if (i == 0) {
+            maior = vetor[i];
+        } else if (vetor[i] > maior) {
+            maior = vetor[i];
+        }
 
-    printf("Digite 5 numeros inteiros:\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Numero %d: ", i + 1);
-        scanf("%d", &numeros[i]);
-    }
-
-    printf("\nNumeros pares digitados:\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (numeros[i] % 2 == 0)
-        {
-            printf("%d ", numeros[i]);
+        if (vetor[i] > 0) {
+            somaPositivos += vetor[i];
         }
     }
 
+    printf("\nNumeros pares identificados: ");
+    for (int i = 0; i < 5; i++) {
+        if (vetor[i] % 2 == 0) {
+            printf("%d ", vetor[i]);
+        }
+    }
+    
+    printf("\nMaior elemento: %d", maior);
+    printf("\nSoma dos valores positivos: %d\n\n", somaPositivos);
+
+    printf("--- Geracao de Tabuada ---\n");
+    do {
+        printf("Digite um numero positivo para a tabuada: ");
+        scanf("%d", &numTabuada);
+        if (numTabuada <= 0) {
+            printf("Erro: A entrada deve ser um numero positivo.\n");
+        }
+    } while (numTabuada <= 0);
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", numTabuada, i, numTabuada * i);
+    }
+
+    printf("\n--- Contagem Regressiva ---\n");
+    do {
+        printf("Digite um numero positivo para iniciar a contagem: ");
+        scanf("%d", &numContagem);
+        if (numContagem <= 0) {
+            printf("Erro: A entrada deve ser um numero positivo.\n");
+        }
+    } while (numContagem <= 0);
+
+    for (int i = numContagem; i >= 0; i--) {
+        printf("%d ", i);
+    }
     printf("\n");
 
     return 0;
