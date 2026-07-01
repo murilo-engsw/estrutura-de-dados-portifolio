d6320387dfec0b2e82416a098e51049bde32d533
Murilo Dahmer da Silva <murilodahmer@alunos.utfpr.edu.br>
2026-07-01 17:31:45 -0300

    feat: corrigindo aula-04

--- aula-04/aula-04.c
+++ aula-04/aula-04.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 
-void troca(int *a, int *b)
-{
+void troca(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-int main()
-{
+int main() {
+    int valor = 10;
+    int *ptr = &valor;
 
-    int x = 10;
-    int y = 20;
+    printf("--- Operadores & e * ---\n");
+    printf("Valor original: %d\n", valor);
+    
+    *ptr = 20; 
+    
+    printf("Valor modificado pelo ponteiro: %d\n\n", valor);
 
-    printf("Antes da troca:\n");
-    printf("x = %d | y = %d\n", x, y);
-
+    int x = 50, y = 100;
+    
+    printf("--- Passagem por Referencia ---\n");
+    printf("Antes da troca: x = %d, y = %d\n", x, y);
+    
     troca(&x, &y);
+    
+    printf("Depois da troca: x = %d, y = %d\n\n", x, y);
 
-    printf("\nDepois da troca:\n");
-    printf("x = %d | y = %d\n", x, y);
+    int arr[3] = {5, 10, 15};
+    
+    printf("--- Ponteiros e Arrays ---\n");
+    for (int i = 0; i < 3; i++) {
+        printf("arr[%d] = %d | *(arr + %d) = %d\n", i, arr[i], i, *(arr + i));
+    }
 
     return 0;
 }
